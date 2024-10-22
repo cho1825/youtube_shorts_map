f83494b53147233257bc6c4fc04211066b49b28b
chs1825 <jhsnotnull@gmail.com>
2024-10-22 18:49:50 +0900

    feat : 기타 설정 commit

--- src/main/java/com/youtube_shorts_map/YoutubeShortsMapApplication.java
+++ src/main/java/com/youtube_shorts_map/YoutubeShortsMapApplication.java
@@ -2,8 +2,10 @@ package com.youtube_shorts_map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YoutubeShortsMapApplication {
 
     public static void main(String[] args) {
