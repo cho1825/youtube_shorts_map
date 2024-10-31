f86ca899ad16405f793947089f2a2bbe71767413
cho1825 <devcho1825@gmail.com>
2024-10-31 23:45:10 +0900

    fix : 영상 수집 갯수 enum 수정

--- src/main/java/com/youtube_shorts_map/collector/enums/ApiFetchLimit.java
+++ src/main/java/com/youtube_shorts_map/collector/enums/ApiFetchLimit.java
@@ -3,13 +3,19 @@ package com.youtube_shorts_map.collector.enums;
 public enum ApiFetchLimit {
 
     ALL,    // 전체 조회
+    SIZE_50,  // 50개 조회
     SIZE_10,  // 10개 조회
-    SIZE_50;  // 50개 조회
+    SIZE_5,  // 5개 조회
+    SIZE_1;  // 1개 조회
 
     public int toInt() {
         switch (this) {
             case ALL:
                 return Integer.MAX_VALUE; // 전체 조회를 의미하는 큰 값
+            case SIZE_1:
+                return 1;
+            case SIZE_5:
+                return 5;
             case SIZE_10:
                 return 10;
             case SIZE_50:
